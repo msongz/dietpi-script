293293dd08fb64efda6e5db8b583670e09e38221
songz <>
2018-03-16 13:52:44 +0800

    refine zerow

--- ins.sh
+++ ins.sh
@@ -2,6 +2,16 @@
 # My first script
 # zerow
 
+GITEA=https://dl.gitea.io/gitea/1.4/gitea-1.4-linux-arm-6
+ARIA=/var/lib/dietpi/dietpi-software/installed
+ZSHURL=https://raw.githubusercontent.com/msongz/oh-my-zsh/master/tools/install.sh
+ZSHSUGG=https://github.com/zsh-users/zsh-autosuggestions.git
+ZSHSYNX=https://github.com/zsh-users/zsh-syntax-highlighting.git
+RSLS=resilio-sync_arm.tar.gz
+RSLK=linux-arm
+NGROK=https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+RETRO=https://github.com/RetroPie/RetroPie-Setup.git
+
 ################### zsh
 echo "-------------zsh screen git"
 
@@ -19,13 +29,11 @@ if ! dpkg -l zsh &> /dev/null
 then
 	sudo apt-get install zsh -y
 fi
-# chsh -s $(grep /zsh$ /etc/shells | tail -1)
-# chsh -s $(which zsh)
 
 echo "-------------oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/msongz/oh-my-zsh/master/tools/install.sh)"
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# sh -c "$(curl -fsSL http://zerow:3000/songz/omz/raw/branch/master/tools/install.sh)"
+
+
+sh -c "$(curl -fsSL $ZSHURL)"
 
 echo "-------------sed"
 sed -i "s/# export PATH=\$HOME\/bin/export PATH=\$HOME\/bin\:\/sbin\:\/usr\/sbin\//g" $HOME/.zshrc
@@ -44,37 +52,34 @@ alias f/=\"find / -type f -iname\" f.=\"find . -type f -iname\" p=\"pidof\" k=\"
 source ~/.iterm2_shell_integration.zsh" >> $HOME/.zshrc
 
 
+
 echo "-------------zsh-autosuggestions"
 if [ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
 	# git clone http://zerow:3000/songz/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-	git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+	git clone $ZSHSUGG $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 fi
 
 echo "-------------zsh-syntax-highlighting"
 if [ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+	git clone $ZSHSYNX $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 	# git clone http://zerow:3000/songz/syntax.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
 
-
-# apt
-echo "-------------apt"
-# sudo apt-get update
-# sudo apt-get upgrade -y
-# sudo apt-get install htop -y
-# sudo apt-get install git -y
 
 # rc.local
 #echo "-------------rc.local"
 #echo -e "#!/bin/sh -e\naria2c --conf-path=/home/pi/.aria2/aria2.conf &\nsu - root -c '/home/pi/rslsync --webui.listen 0.0.0.0:8888' &\nsu - pi -c 'python -m SimpleHTTPServer 8080' &\nexit 0" | sudo tee /etc/rc.local
 
+
 #################### aria2
 echo "-------------aria2"
+
+
+touch $ARIA/aria2.session
+
 # git clone https://github.com/ziahamza/webui-aria2.git
 # sudo apt-get install aria2 -y
 # mkdir ~/.aria2
-# touch ~/.aria2/aria2.session
-# echo -e "dir=/home/pi/Downloads\ncontinue=true\nenable-rpc=true\nrpc-allow-origin-all=true\nrpc-listen-all=true\nrpc-listen-port=6800\nmax-connection-per-server=10\nsplit=10\nmax-overall-download-limit=0\nmax-download-limit=0\nmax-overall-upload-limit=0\nmax-upload-limit=0\ninput-file=/home/pi/.aria2/aria2.session\nsave-session=/home/pi/.aria2/aria2.session\nsave-session-interval=60\nseed-ratio=1\nbt-seed-unverified=true" > ~/.aria2/aria2.conf
 # mkdir -p $HOME/.config/aria2/ && touch $_/aria2.session $_/aria2.conf
 # mkdir $HOME/.config/aria2/aria2 && $_/aria2.conf
 
@@ -98,8 +103,8 @@ pause=false
 rpc-allow-origin-all=true
 
 # SAVE SESSION
-input-file=/var/lib/dietpi/dietpi-software/installed/aria2.session
-save-session=/var/lib/dietpi/dietpi-software/installed/aria2.session
+input-file=$ARIA/aria2.session
+save-session=$ARIA/aria2.session
 save-session-interval=60
 
 # General Options
@@ -131,18 +136,14 @@ metalink-servers=13
 allow-overwrite=false
 always-resume=true
 auto-file-renaming=false
-file-allocation=none" > /var/lib/dietpi/dietpi-software/installed/aria2.conf
-
-# ftp
-# echo "-------------ftp"
-# sudo apt-get install pure-ftpd -y
+file-allocation=none" > $ARIA/aria2.conf
 
 #################### gitea
 echo "-------------gitea"
+
 if [ ! -d $HOME/gitea ]; then
-# export http_proxy=192.168.1.40:1087 https_proxy=192.168.1.40:1087
 mkdir -p $HOME/gitea
-wget -O $HOME/gitea/gitea https://dl.gitea.io/gitea/1.4/gitea-1.4-linux-arm-6
+wget -O $HOME/gitea/gitea $GITEA
 chmod +x $HOME/gitea/gitea
 echo -e "[Unit]
 Description=Gitea (Git with a cup of tea)
@@ -162,13 +163,10 @@ fi
 ################### resilio
 echo "-------------resilio"
 
-RSLS=resilio-sync_arm.tar.gz
-LINK=linux-arm
 
 if [ ! -d $HOME/resilio ]; then
-# export http_proxy=192.168.1.40:1087 https_proxy=192.168.1.40:1087
 mkdir -p $HOME/resilio
-wget https://download-cdn.resilio.com/stable/$LINK/$RSLS
+wget https://download-cdn.resilio.com/stable/$RSLK/$RSLS
 tar -xf $RSLS -C $HOME/resilio
 sudo touch /etc/systemd/system/resilio.service
 
@@ -189,34 +187,10 @@ WantedBy = multi-user.target" | sudo tee /etc/systemd/system/resilio.service
 
 sudo systemctl enable resilio.service
 fi
-# samba
-# echo "-------------samba"
-# sudo apt-get install samba samba-common-bin -y
-# echo -e "[downloads]\ncomment = downloads\npath = "/home/pi/Downloads"\nwriteable = yes\nguest ok = yes\ncreate mask = 0644\ndirectory mask = 0755\nforce user = pi" | sudo tee --append /etc/samba/smb.conf
-
-# chk-rsl.sh
-# echo "-------------chk-rsl"
-# echo -e "#!/bin/bash\npidof rslsync > /dev/null\nif [ \$? -ne 0 ] ; then\n    /home/pi/rslsync --webui.listen 0.0.0.0:8888 &\nfi" > /home/pi/chk-rsl.sh
-# chmod 777 /home/pi/chk-rsl.sh
-
-
-# sudo crontab
-# echo "-------------sudo crontab"
-# (crontab -l ; echo "*/5 * * * * sh /home/pi/chk-rsl.sh") | sudo crontab -
-# node red
-# echo "-------------node-red"
-# bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
-# echo ";;;;;;;;;dashboard"
-# cd .node-red && npm i node-red-dashboard
-# sudo systemctl enable nodered.service
-
 
 echo "-------------source-list"
 echo -e "#deb https://archive.raspberrypi.org/debian/ stretch main ui
 deb http://mirrors.ustc.edu.cn/archive.raspberrypi.org/debian/ stretch main ui" > /etc/apt/sources.list.d/raspi.list
-# aria2 132
-# 62
-# nodered 122
 
 ################### ngrok
 
@@ -225,7 +199,8 @@ if [ ! -f $HOME/ngrok ]
 then
 echo "-------------ngrok not enable"
 
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+
+wget $NGROK
 
 unzip $HOME/ngrok-stable-linux-arm.zip
 
@@ -270,6 +245,7 @@ Restart=always
 WantedBy=multi-user.target" | sudo tee /etc/systemd/system/ngrok.service
 
 # sudo systemctl enable ngrok.service
+
 fi
 
 ################### RetroPie
@@ -282,27 +258,24 @@ sudo apt-get install lsb-release -y
 fi
 
 if [ ! -d $HOME/RetroPie-Setup ]; then
-# git clone --depth=1 http://zerow:3000/songz/RetroPie-Setup.git
-git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
+git clone --depth=1 $RETRO
 fi
 
-cd $HOME/RetroPie-Setup
-sudo ./retropie_setup.sh
+sudo $HOME/RetroPie-Setup/retropie_setup.sh
 
+################### finish
 
 echo "-------------source"
 # source $HOME/.zshrc
 env zsh
 
-# finish
-echo "-------------finish"
 
 # useful command
 
 # cat Documents/ins.sh | ssh root@pi2 "cat > ins.sh"
 
-#-##### omxplayer looping
+# omxplayer looping
 
 # while : ; do for f in * ; do omxplayer --blank --win 0,40,1280,680 --aspect-mode stretch "$f"; done; done
 
-#-#####
+#
