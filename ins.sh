398f5d319f3cc807b2cde86f9a85f049331bd022
songz <songzmeng@gmail.com>
2019-01-08 14:53:31 +0800

    remote-host init

--- ins.sh
+++ ins.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 # My first script
-# pi2
+# remote-host
 
-GITEA=https://dl.gitea.io/gitea/1.6.2/gitea-1.6.2-linux-arm-7
-ARIA=/var/lib/dietpi/dietpi-software/installed
-EPAD=https://github.com/ether/etherpad-lite.git
+# GITEA=https://dl.gitea.io/gitea/1.6.2/gitea-1.6.2-linux-arm-7
+# ARIA=/var/lib/dietpi/dietpi-software/installed
+# EPAD=https://github.com/ether/etherpad-lite.git
 ZSHURL=https://raw.githubusercontent.com/msongz/oh-my-zsh/master/tools/install.sh
 ZSHSUGG=https://github.com/zsh-users/zsh-autosuggestions.git
 ZSHSYNX=https://github.com/zsh-users/zsh-syntax-highlighting.git
-POWERLEVEL9K=https://github.com/bhilburn/powerlevel9k.git
+# POWERLEVEL9K=https://github.com/bhilburn/powerlevel9k.git
 RSSHUB=https://github.com/DIYgod/RSSHub.git
-RSLS=resilio-sync_armhf.tar.gz
-RSLK=linux-armhf
+RSLS=resilio-sync_x64.tar.gz
+RSLK=linux-x64
 # NGROK=https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
-RETRO=https://github.com/RetroPie/RetroPie-Setup.git
-FRPLINK=https://github.com/fatedier/frp/releases/download/v0.21.0/frp_0.21.0_linux_arm.tar.gz
+# RETRO=https://github.com/RetroPie/RetroPie-Setup.git
+FRPLINK=https://github.com/fatedier/frp/releases/download/v0.22.0/frp_0.22.0_linux_amd64.tar.gz
+
+
+
+
 
 ################### zsh
-echo "-------------zsh screen git"
-
-if ! dpkg -l screen &> /dev/null
-then
-	sudo apt-get install screen -y
-fi
+echo "-------------zsh screen git curl"
 
 if ! dpkg -l git &> /dev/null
 then
 	sudo apt-get install git -y
 fi
+
+
+sudo apt-get install curl -y
+
 
 if ! dpkg -l zsh &> /dev/null
 then
@@ -36,27 +39,21 @@ fi
 
 echo "-------------oh-my-zsh"
 
-sh -c "$(curl -fsSL $ZSHURL)"
+sh -c "$(wget $ZSHURL -O -)"
 
 echo "-------------zshrc"
 
 sed -i "s/# export PATH=\$HOME\/bin/export PATH=\$HOME\/bin\:\/sbin\:\/usr\/sbin\//g" $HOME/.zshrc
 sed -i "s/# export LANG/export LANG/g" $HOME/.zshrc
 sed -i "s/git$/git colorize sudo extract zsh-autosuggestions z zsh-syntax-highlighting/g" $HOME/.zshrc
-sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel9k\/powerlevel9k\"/g" $HOME/.zshrc
+# sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel9k\/powerlevel9k\"/g" $HOME/.zshrc
 sed -i "s/^alias/#alias/g" $HOME/.zshrc
-sed -i "s/^\/DietPi/#\/DietPi/g" $HOME/.zshrc
-sed -i "s/^\. \/DietPi/#\. \/DietPi/g" $HOME/.zshrc
-sed -i "s/^source ~\/\.iterm2/#source ~\/\.iterm2/g" $HOME/.zshrc
+# sed -i "s/^\/DietPi/#\/DietPi/g" $HOME/.zshrc
+# sed -i "s/^\. \/DietPi/#\. \/DietPi/g" $HOME/.zshrc
+# sed -i "s/^source ~\/\.iterm2/#source ~\/\.iterm2/g" $HOME/.zshrc
 sed -i "s/^echo/#echo/g" $HOME/.zshrc
 
-echo -e "/DietPi/dietpi/login
-. /DietPi/dietpi/func/dietpi-globals
-alias json=\"python -m json.tool\" css=\"sudo systemctl stop\" csr=\"sudo systemctl restart\" cst=\"sudo systemctl status\" f/=\"find / -type f -iname\" f.=\"find . -type f -iname\" p=\"pidof\" k=\"kill -9\" rr=\"sudo reboot\" is=\"sudo apt-get install\" up=\"sudo apt-get update\" ud=\"sudo apt-get upgrade\" sd=\"sudo poweroff\" hh=\"htop\"
-source ~/.iterm2_shell_integration.zsh
-echo -e \" \\\033[1mstorage left : \\\033[7m\$(df -h|grep '/dev/root'|awk '{ print \$4 }')\"" >> $HOME/.zshrc
-
-
+echo -e "alias json=\"python -m json.tool\" css=\"sudo systemctl stop\" csr=\"sudo systemctl restart\" cst=\"sudo systemctl status\" f/=\"find / -type f -iname\" f.=\"find . -type f -iname\" p=\"pidof\" k=\"kill -9\" rr=\"sudo reboot\" is=\"sudo apt-get install\" up=\"sudo apt-get update\" ud=\"sudo apt-get upgrade\" sd=\"sudo poweroff\" hh=\"htop\"" >> $HOME/.zshrc
 
 echo "-------------zsh-autosuggestions"
 
@@ -72,11 +69,10 @@ if [ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
 	# git clone http://zerow:3000/songz/syntax.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
 
-echo "-------------powerlevel9k"
+################### nodejs
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
-if [ ! -d $HOME/.oh-my-zsh/custom/themes/powerlevel9k ]; then
-  git clone $POWERLEVEL9K $HOME/.oh-my-zsh/custom/themes/powerlevel9k
-fi
 
 
 
@@ -85,102 +81,13 @@ fi
 #echo -e "#!/bin/sh -e\naria2c --conf-path=/home/pi/.aria2/aria2.conf &\nsu - root -c '/home/pi/rslsync --webui.listen 0.0.0.0:8888' &\nsu - pi -c 'python -m SimpleHTTPServer 8080' &\nexit 0" | sudo tee /etc/rc.local
 
 
-#################### aria2
-
-echo "-------------aria2"
-
-touch $ARIA/aria2.session
-
-# git clone https://github.com/ziahamza/webui-aria2.git
-# sudo apt-get install aria2 -y
-# mkdir ~/.aria2
-# mkdir -p $HOME/.config/aria2/ && touch $_/aria2.session $_/aria2.conf
-# mkdir $HOME/.config/aria2/aria2 && $_/aria2.conf
-
-echo -e "# host is where aria2c is running on
-# host=localhost
-dir=/mnt/dietpi_userdata/downloads
-
-# cleanup_policy
-# cleanup-policy=clean_got
-# cleanup-percent=90%
-
-# The fallowing options are aria2c options as usual aria2.conf file
-# https://aria2.github.io/manual/en/html/aria2c.html
-
-# RPC Options
-enable-rpc=true
-rpc-listen-all=true
-rpc-listen-port=6800
-rpc-secret=147852
-pause=false
-rpc-allow-origin-all=true
-
-# SAVE SESSION
-input-file=$ARIA/aria2.session
-save-session=$ARIA/aria2.session
-save-session-interval=60
-
-# General Options
-log=/var/log/aria2.log
-log-level=warn
-
-split=10
-continue=true
-check-integrity=true
-check-certificate=false
-max-concurrent-downloads=10
-max-connection-per-server=10
-max-file-not-found=3
-max-tries=5
-retry-wait=60
-ftp-pasv=true
-bt-max-peers=13
-# listen-port=6881-6999
-load-cookies=true
-
-max-overall-upload-limit=0
-max-overall-download-limit=0
-max-upload-limit=0
-max-download-limit=0
-seed-ratio=1
-seed-time=5000
-
-# metalink-servers=13
-allow-overwrite=false
-always-resume=true
-auto-file-renaming=false
-file-allocation=none" > $ARIA/aria2.conf
-
-#################### gitea
-
-echo "-------------gitea"
-
-if [ ! -d $HOME/gitea ]; then
-mkdir -p $HOME/gitea
-wget -O $HOME/gitea/gitea $GITEA
-chmod +x $HOME/gitea/gitea
-echo -e "[Unit]
-Description=Gitea (Git with a cup of tea)
-
-[Service]
-
-User=root
-WorkingDirectory=$HOME/gitea
-ExecStart=$HOME/gitea/gitea web
-Restart=always
-
-[Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/gitea.service
-
-sudo systemctl enable gitea.service
-fi
-################### resilio
+#################### resilio
 
 echo "-------------resilio"
 
 if [ ! -d $HOME/resilio ]; then
 mkdir -p $HOME/resilio
+
 wget https://download-cdn.resilio.com/stable/$RSLK/$RSLS
 tar -xf $RSLS -C $HOME/resilio
 sudo touch /etc/systemd/system/resilio.service
@@ -203,65 +110,6 @@ WantedBy = multi-user.target" | sudo tee /etc/systemd/system/resilio.service
 sudo systemctl enable resilio.service
 fi
 
-echo "-------------source-list"
-
-echo -e "#deb https://archive.raspberrypi.org/debian/ stretch main ui
-deb http://mirrors.ustc.edu.cn/archive.raspberrypi.org/debian/ stretch main ui" > /etc/apt/sources.list.d/raspi.list
-
-################### ngrok
-
-
-# if [ ! -f $HOME/ngrok ]
-# then
-# echo "-------------ngrok"
-
-# wget $NGROK
-
-# unzip $HOME/ngrok-stable-linux-arm.zip
-
-# mkdir -p $HOME/.ngrok2 && touch $_/ngrok.yml
-
-# echo -e "authtoken: 2jWq5RHKhqQmZ9ieHQh8n_FPQFRU67yxYey4NNBWja
-# web_addr: 0.0.0.0:4040
-# tunnels:
-#   httpbin:
-#     proto: http
-#     addr: 6800
-#     bind_tls: true
-#   demo:
-#     proto: http
-#     addr: 3000
-#     bind_tls: true
-#     inspect: false
-#     auth: demo:secret
-#   songz:
-#     proto: http
-#     bind_tls: true
-#     addr: 8888
-#   httpbinaa:
-#     proto: http
-#     addr: 80
-#     bind_tls: true" > $HOME/.ngrok2/ngrok.yml
-
-# echo -e "[Unit]
-# Description=ngrok
-# After=network.target
-
-# [Service]
-
-# Type=simple
-# Restart=always
-# User=root
-# WorkingDirectory=$HOME
-# ExecStart=$HOME/ngrok start -all
-
-# [Install]
-# WantedBy=multi-user.target" | sudo tee /etc/systemd/system/ngrok.service
-
-# # sudo systemctl enable ngrok.service
-
-# sudo systemctl enable ngrok.service
-# fi
 
 ################### frp
 
@@ -272,7 +120,7 @@ if [ ! -d $HOME/frp ]; then
 
 
 echo -e "[common]
-server_addr = msongz.design
+server_addr = msongz.ml
 server_port = 7000
 
 [ssh]
@@ -284,7 +132,7 @@ remote_port = 6000
 [web]
 type = http
 local_port = 80
-custom_domains = msongz.design" | sudo tee $HOME/frp/frpc.ini
+custom_domains = msongz.ml" | sudo tee $HOME/frp/frpc.ini
 
 echo -e "[Unit]
 Description=frp
@@ -313,7 +161,7 @@ if [ ! -d $HOME/RSSHub ]; then
 git clone $RSSHUB
 
 
-npm config set registry https://registry.npm.taobao.org
+# npm config set registry https://registry.npm.taobao.org
 
 npm i -g npm
 
@@ -326,7 +174,7 @@ Description=rsshub
 [Service]
 User=root
 WorkingDirectory=$HOME/RSSHub
-ExecStart=/usr/local/bin/node $HOME/RSSHub/index.js
+ExecStart=/usr/bin/npm start
 Restart=always
 
 [Install]
@@ -336,121 +184,10 @@ sudo systemctl enable rss.service
 fi
 
 
-################### etherpad
-
-echo "-------------etherpad"
-
-if [ ! -d $HOME/etherpad-lite ]; then
-
-git clone $EPAD
-
-$HOME/etherpad-lite/bin/installDeps.sh --root
-
-echo -e "[Unit]
-Description=etherpad-lite
-
-[Service]
-User=root
-Environment=NODE_ENV=production
-ExecStart=$HOME/etherpad-lite/bin/run.sh
-Restart=always
-
-[Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/etherpad.service
-
-sudo systemctl enable etherpad.service
-
-sed -i "s/^bin\/installDeps/#bin\/installDeps/g" $HOME/etherpad-lite/bin/run.sh
-sed -i "s/etherpaduser/root/g" $HOME/etherpad-lite/settings.json
-sed -i "s/changeme1//" $HOME/etherpad-lite/settings.json
-sed -i "80s/.*/  \/\*/g" $HOME/etherpad-lite/settings.json
-sed -i "85s/.*/  \*\//g" $HOME/etherpad-lite/settings.json
-sed -i "92s/.*//g" $HOME/etherpad-lite/settings.json
-sed -i "102s/.*//g" $HOME/etherpad-lite/settings.json
-sed -i "310s/.*//g" $HOME/etherpad-lite/settings.json
-sed -i "323s/.*//g" $HOME/etherpad-lite/settings.json
-sed -i "s/PASSWORD//g" $HOME/etherpad-lite/settings.json
-
-
-mysql -u root -e "create database etherpad_lite_db"
-
-fi
-
-
-
-
-################### RetroPie
-
-
-if ! dpkg -l lsb-release &> /dev/null
-then
-echo "-------------RetroPie"
-
-sudo apt-get install lsb-release -y
-fi
-
-if [ ! -d $HOME/RetroPie-Setup ]; then
-git clone --depth=1 $RETRO
-
-
-sudo $HOME/RetroPie-Setup/retropie_setup.sh
-fi
-################### finish
-
 echo "-------------source"
 
 # source $HOME/.zshrc
 env zsh
 
 
-
-
-# useful command
-
-# cat /Users/meng/dietpi-script/songz-dietpi.txt >/Volumes/boot/dietpi.txt
-
-# cat Documents/ins.sh | ssh root@pi2 "cat > ins.sh"
-
-# omxplayer looping
-
-# while : ; do for f in * ; do omxplayer --blank --win 0,40,1280,680 --aspect-mode stretch "$f"; done; done
-
-# lighttpd wordpress fix permalink to /etc/lighttpd/lighttpd.conf
-
-# server.modules += ("mod_rewrite")
-# url.rewrite = (
-#         "^/wordpress/(wp-admin|wp-includes|wp-content|gallery2)/(.*)" => "$0",
-#         "^/wordpress/(.*)\.(.+)$" => "$0",
-#         "^/wordpress/(.+)/?$" => "/wordpress/index.php/$1"
-# )
-
-#
-#
-#[Unit]
-# Description=frp
-# After=network.target
-
-# [Service]
-# Type=simple
-# Restart=always
-# User=root
-# WorkingDirectory=/root/frp
-# ExecStart=/root/frp/frps -c /root/frp/frps.ini
-
-# [Install]
-# WantedBy=multi-user.target
-# 
-# 
-# 
-# [Unit]
-# Description=rsshub
-
-# [Service]
-# User=root
-# WorkingDirectory=/root/RSSHub
-# ExecStart=/usr/bin/node /root/RSSHub/index.js
-# Restart=always
-
-# [Install]
-# WantedBy=multi-user.target
 
